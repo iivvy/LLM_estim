506432e4e8fda5075fc66a1545b9cdcad65ca16d
Nixi <frraroua@gmail.com>
2024-12-16 23:30:08 +0100

    LLM response model

--- model.py
+++ model.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 class ProjectInput(BaseModel):
     project_description : str
     user_stories : list[str]
+
+
+class LLMResponse(BaseModel):
+    duration: float = Field(..., description="Estimated duration in years")
+    cost: int = Field(..., description="Estimated cost in dollars")
+    explanation: str
